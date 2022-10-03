5d70f155f092e13bde30b7dc9f557428892c14f3
P. H <haeusser@gmx.de>
2022-10-03 11:53:04 +0200

    Add files via upload

---
+++ TwoThreadConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace TwoThreadConsoleApp
+{
+    internal class Program
+    {
+        static int  QueueSize = 1000;
+        static string InputFile = "input.txt";
+        static string OutputFile = "output.txt";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // split task in two threads, one reader, one writer
+            // output is in sync with input
+            var multi = new TwoThread(QueueSize);
+
+            multi.ProcessFileAsync(InputFile, OutputFile, processLine);
+
+            multi.Dispose();
+                        
+            stopwatch.Stop();
+
+            TimeSpan ts = stopwatch.Elapsed;
+
+            var elapsedTime = string.Format("Runtime {0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+            Console.WriteLine(elapsedTime);
+            Console.WriteLine("Done. Press any key to exit");
+            Console.ReadKey();
+
+        }
+
+        //simple example, return reverse line
+        private static string processLine(string arg)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = arg.Length-1; i >=0 ; i--)
+            {
+                sb.Append(arg[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TwoThreadConsoleApp/TwoThread.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TwoThreadConsoleApp
+{
+    public class TwoThread : IDisposable
+    {
+        private int QueueSize = 10000;
+        private BlockingCollection<string> _inputQueue;
+        private BlockingCollection<string> _outputQueue;
+
+        public TwoThread()
+        {
+            _inputQueue = new BlockingCollection<string>(QueueSize);
+            _outputQueue = new BlockingCollection<string>(QueueSize);
+        }
+
+        public TwoThread(int queueSize)
+        {
+            QueueSize = queueSize;
+            _inputQueue = new BlockingCollection<string>(QueueSize);
+            _outputQueue = new BlockingCollection<string>(QueueSize);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _inputQueue.Dispose();
+                _outputQueue.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+        internal Task CreateReader(string inputFilename)
+        {
+            var task = Task.Factory.StartNew(() =>
+                ReadInput(inputFilename), TaskCreationOptions.LongRunning);
+
+            return task;
+        }
+
+        internal Task CreateWriter(string outputFilename)
+        {
+            return Task.Factory.StartNew(() =>
+                WriteOutput(outputFilename), TaskCreationOptions.LongRunning);
+        }
+
+        public void ProcessFileAsync(string inputFilename, string outputFilename,
+            Func<string, string> processLine)
+        {
+            var reader = Task.Factory.StartNew(() =>
+                ReadInput(inputFilename), TaskCreationOptions.LongRunning);
+
+            var writer = Task.Factory.StartNew(() =>
+                WriteOutput(outputFilename), TaskCreationOptions.LongRunning);
+
+            foreach (var line in _inputQueue.GetConsumingEnumerable())
+            {
+                var newline = processLine(line);
+                _outputQueue.Add(newline);
+            }
+
+            _outputQueue.CompleteAdding();
+            Task.WaitAll(reader, writer);
+        }
+
+        private void ReadInput(string inputFilename)
+        {
+            foreach (var line in File.ReadLines(inputFilename))
+            {
+                _inputQueue.Add(line);
+            }
+
+            _inputQueue.CompleteAdding();
+        }
+
+        private void WriteOutput(string outputFilename)
+        {
+            using (var outputFile = new StreamWriter(outputFilename))
+            {
+                foreach (var line in _outputQueue.GetConsumingEnumerable())
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+        }
+    }
+}
